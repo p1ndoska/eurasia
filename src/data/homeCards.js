4cb65a5f0f295c445969be266dccc02b076d301b
Анна Ковалева <akovaleva@ban.by>
2026-09-08 16:26:06 +0300

    Use SVG icons for cards

--- src/data/homeCards.js
+++ src/data/homeCards.js
@@ -1,9 +1,9 @@
-import program from '../images/icons/program-checklist.png'
+import program from '../images/icons/4544845-business-comerce-delivery-list-shop_121445.svg'
 import participants from '../images/icons/people_group_icon_188185.svg'
 import hotel from '../images/icons/company_workplace_building_office_icon_262568.svg'
-import sponsors from '../images/icons/sponsors-handshake.png'
+import sponsors from '../images/icons/cooperation_greeting_partnership_deal_agreement_handshake_icon_262591.svg'
 import gallery from '../images/icons/mbriimagegallery_99572.svg'
-import contacts from '../images/icons/contacts-person.png'
+import contacts from '../images/icons/person_avatar_account_user_icon_191606.svg'
 
 const homeCards = [
   {
